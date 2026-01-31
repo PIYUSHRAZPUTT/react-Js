fcc60c7987d1e5e57c6704600114c34224693749
PIYUSHRAZPUTT <piyushrajpoot1488999@gmail.com>
2026-01-31 23:13:37 +0530

    my first react program

---
+++ react-app1/src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>This is my first react program.</h1>
+      <h3>Myself Piyush.</h3>
+    </div>
+  )
+}
+
+export default App
