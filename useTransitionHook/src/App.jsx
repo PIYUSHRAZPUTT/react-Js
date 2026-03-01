ffe567463e9fd3c81327e1380bfac40287472a4e
PIYUSHRAZPUTT <piyushrajpoot1488999@gmail.com>
2026-03-01 15:03:12 +0530

    useTransitionHook

---
+++ useTransitionHook/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [input, setInput] = useState("");
+  
+
+  return (
+    <>
+      <input type="text"
+      value={} />
+    </>
+  )
+}
+
+export default App
